56d63ce94570d287433d5f6e3262d7a436112000
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2016-09-12 12:17:43 +0800

    clean codes

--- utils/path.go
+++ utils/path.go
@@ -1,24 +1,12 @@
 package utils
 
 import (
-	//"github.com/mitchellh/go-homedir"
 	"os"
-	//"path/filepath"
 )
 
 var deckServerRoot = "http://104.199.175.27/deck"
 var deckLocalBinPath = "/usr/local/bin"
 var deckLocalSystemUnitPath = "/etc/systemd/system"
-
-func init() {
-	//homeDir, err := homedir.Dir()
-	//if err != nil {
-	//	panic(err)
-	//}
-	// consider using /usr/local/bin
-	//deckLocalRootPath := filepath.Join(homeDir, ".deck")
-	//deckLocalBinPath = filepath.Join(deckLocalRootPath, "bin")
-}
 
 func MkdirDeckLocalBinPath() {
 	if err := os.MkdirAll(deckLocalBinPath, 0755); err != nil {
